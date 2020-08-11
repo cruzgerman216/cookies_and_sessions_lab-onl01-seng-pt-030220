a869a77e6e41e268ef9a10dfd45fda3478262a91
German Cruz <cruzgerman216@yahoo.com>
2020-08-11 01:41:08 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
-class ProductsController < ActionController::Base
+class ProductsController < ApplicationController
 
   def index
-
+    @cart = cart
   end
 
   def add
-
+    cart << session[:product]
+    puts cart
+    puts "TEST"
+    render :index
   end
 end
